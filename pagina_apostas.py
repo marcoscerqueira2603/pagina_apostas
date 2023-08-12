5bb62e4bfdc5f316fe5c386e570b547ad4420e85
marcoscerqueira2603 <133461862+marcoscerqueira2603@users.noreply.github.com>
2023-08-12 15:55:03 -0300

    Add files via upload

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -18,12 +18,13 @@ placar_b = st.number_input('Placar B', min_value=0)
 
 if st.button('Inserir'):
     new_row = {'Time A': time_a, 'Placar A': placar_a, 'Time B': time_b, 'Placar B': placar_b}
-    df = df.append(new_row, ignore_index=True)
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
     df.to_csv(data_file, index=False)
     st.success('Dados inseridos com sucesso!')
 
 # Mostra os dados inseridos
 st.title('Dados de Jogos de Futebol')
 st.dataframe(df)
+
 
 
