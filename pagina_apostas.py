a902e6153930ad1bc61063526f292a238cbf81b7
marcoscerqueira2603 <133461862+marcoscerqueira2603@users.noreply.github.com>
2023-08-25 22:23:28 -0300

    Add files via upload

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -10,7 +10,7 @@ from openpyxl import load_workbook
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_excel(csv_url)
+    return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
 
