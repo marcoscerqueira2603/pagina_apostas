8aa2ead9e5ad82c85493601d90fdf1b8a7c21050
marcoscerqueira2603 <133461862+marcoscerqueira2603@users.noreply.github.com>
2023-08-25 23:12:39 -0300

    Add files via upload

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -509,12 +509,13 @@ with tab1:
     # Adicionar as novas linhas ao DataFrame
     if novas_linhas:
         novas_linhas_df = pd.DataFrame(novas_linhas, columns=tendencias.columns)
-        tendencias = tendencias.append(novas_linhas_df, ignore_index=True)
+        tendencias = pd.concat([tendencias, novas_linhas_df], ignore_index=True)
 
         # Atualizar a planilha com as novas linhas
         worksheet = client.open_by_url("your_google_sheets_url").get_worksheet(0)
         worksheet.clear()  # Limpar o conteúdo atual da planilha
-        worksheet.insert_rows(tendencias.values.tolist(), 2)  # Inserir as linhas atualizadas
+        values_to_insert = [tendencias.columns.tolist()] + tendencias.values.tolist()  # Inserir cabeçalho e dados
+        worksheet.insert_rows(values_to_insert, 2)  # Inserir as linhas atualizadas
 
         # Notificar o usuário sobre a atualização bem-sucedida
         st.success("Dados adicionados com sucesso à planilha!")
