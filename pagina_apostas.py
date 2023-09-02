0fd6fc194db2fb4ed0079ea36d4194e0ae566506
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2023-09-02 20:57:43 -0300

    Update pagina_apostas.py

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -732,6 +732,7 @@ with tab2:
     entradas['Data'] = pd.to_datetime(entradas['Data'], errors='coerce')
     entradas['Mês'] = entradas['Data'].dt.strftime('%b')
     entradas['Retorno - reduzido a odd'] = entradas.apply(lambda row: row['Odd'] if row['Resultado'] == 1 else 0, axis=1)
+    entradas['Retorno - reduzido a odd'] = entradas['Retorno - reduzido a odd'].astype(float)
     entradas = entradas[entradas['Aposta Anulada?'] != "Sim"]
     
     investimento_total = entradas['Investimento'].sum()
