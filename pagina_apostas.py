7d834ea3869746e631eb21d6e7e4ef54a4f2b3f0
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-01-14 14:13:29 -0300

    aaa

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -10,6 +10,8 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from tkinter import *
+from tkinter import ttk
 
 
 #import json
