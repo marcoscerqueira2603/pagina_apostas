0ec389c16474a408e4930f82375848dbffb4087a
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-01-20 17:41:46 -0300

    aa

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -853,5 +853,5 @@ with tab2:
 
 
     apostas_feitas = entradas.groupby('Mês').agg({'Investimento': 'count', 'Resultado': 'sum'}).reset_index()
-    apostas_feitas['% de Aproveitamento'] = round(((apostas_feitas['Resultado'] /apostas_feitas['Investimento']) - 1) * 100, 2)
-    apostas_feitas
+    apostas_feitas['% de Aproveitamento'] = round(((apostas_feitas['Resultado'] /apostas_feitas['Investimento'])) * 100, 2)
+    apostas_feitas.rename(columns={'Investimento': 'Qtd de Apostas', 'Resultado': 'Qtd de Acertos'}, inplace=True)
