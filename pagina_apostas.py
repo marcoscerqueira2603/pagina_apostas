62c1a04eb6fd1ae3664aebb6e1973424725cbc2f
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-01-21 16:01:45 -0300

    aaaa

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -941,7 +941,7 @@ with tab2:
 
     tendencias_2linhas_filtrada
     tendencias_mes = tendencias_2linhas_filtrada.pivot_table(index='Mês', columns='Tipo de Linha', values='Bateu', aggfunc='mean').reset_index()
-    tendencias_mes.loc[:, 'Total'] = tendencias_mes.mean(axis=1)4
+    tendencias_mes.loc[:, 'Total'] = tendencias_mes.mean(axis=1)
     tendencias_mes *= 100
     tendencias_mes.columns = ['Mês', '% Cantos', '% Gols', '% Total']
     tendencias_mes
