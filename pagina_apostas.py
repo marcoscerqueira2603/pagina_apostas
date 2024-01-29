d9b499b7e262aebfda61d65b7484a92adee01622
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-01-28 23:58:06 -0300

    aaaa

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -947,7 +947,7 @@ with tab2:
 
     tendencias_2linhas_filtrada_aprov = tendencias_2linhas_filtrada.pivot_table(index='Pais', columns='Tipo de Linha', values='Bateu', aggfunc='mean').reset_index()
     media_bateu_por_pais = tendencias_2linhas_filtrada.groupby('Pais')['Bateu'].mean()
-    media_bateu_por_pais = media_bateu_por_pais.rename('Bateu', 'Total')
+    media_bateu_por_pais['Total'] = media_bateu_por_pais['Bateu']
     media_bateu_por_pais 
     tendencias_2linhas_filtrada_aprov = pd.merge(tendencias_2linhas_filtrada_aprov, media_bateu_por_pais, how='left', on='Pais')   
     #tendencias_2linhas_filtrada_aprov.loc[:, 'Total'] = tendencias_2linhas_filtrada_aprov.iloc[:, 1:].mean(axis=1)
