8b445c0a23248ea601c81d53488235558aafd6af
marcoscerqueira2603 <marcos.cerqueira087@gmail.com>
2024-02-12 15:01:21 -0300

    aaaa

--- pagina_apostas.py
+++ pagina_apostas.py
@@ -628,7 +628,7 @@ with tab1:
     prob_a = round(1/(1- chance_time_a),2)
     prob_b = round(1/(1- chance_time_b),2)
     odd_bet_2gols = st.text_input('Odd Bet 2 gols')
-
+    odd_bet_2gols
     st.title('Análise 1.5')
     col1, col2= st.columns([0.5, 0.5,])
     with col1:
